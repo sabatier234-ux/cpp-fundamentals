69854d8c0bed0e09357508ea55ba5e276fd1767e
Saba <sabatier234@gmail.com>
2026-01-16 19:59:58 +0500

    add switch case example

---
+++ control structures/switch-case.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num;
+	cout << "enter a number of month between 1 and 12" << endl;
+	cin >> num;
+		switch (num) {
+		case 1:
+			cout << "january" << endl;
+			break;
+			case 2:
+			cout << "february" << endl;
+			break;
+			case 3:
+			cout << "march" << endl;
+			break;
+			case 4:
+			cout << "april" << endl;
+			break;
+			case 5:
+				cout << "may" << endl;
+				break;
+			case 6:
+				cout << "june" << endl;
+				break;
+			case 7:
+				cout << "july" << endl;
+				break;
+			case 8:
+				cout << "august" << endl;
+				break;
+			case 9:
+				cout << "september" << endl;
+				break;
+			case 10:
+				cout << "october" << endl;
+			    break;
+			case 11:
+				cout << "november"<< endl;
+			   break;
+			case 12:
+			 cout << "december" << endl;
+				break;
+			default:
+				cout << "enter valid month" <<endl;
+
+		}
+		system("pause");
+		return 0;
+	
+
+}
